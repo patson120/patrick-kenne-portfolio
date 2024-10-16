d64f509e86719f9e255f8e5c0a0f144bacb54ad3
patson120 <patrick1kenne@gmail.com>
2024-10-16 15:33:37 +0100

    Correction du bug du menu de navigation mobile

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,17 +1,8 @@
 'use client'
-
 import { useEffect, useState } from 'react'
 
 const NavigationBar = () => {
-
   const [showMenu, setshowMenu] = useState(false)
-
-  globalThis?.window?.addEventListener("resize", () => {
-    if (window.innerWidth > 1024) {
-      setshowMenu(false)
-      document.getElementById('mobile-menu')?.classList.remove('hidden')
-    }
-  })
 
   const handleToggleMenu = () => {
     setshowMenu(prev => !prev)
@@ -34,21 +25,21 @@ const NavigationBar = () => {
   }, [])
 
   return (
-    <header className="bg-white shadow-md">
+    <header className="bg-white flex-1 flex-col fixed top-0 left-0 right-0 z-[99999] shadow-lg">
       <nav className="container mx-auto px-4 py-6 flex justify-between items-center">
-        {/* <h1 className="text-2xl font-heading font-bold text-primary">Patrick KENNE</h1> */}
-        <span></span>
-        <ul className="hidden md:flex space-x-6">
+        <h1 className="text-2xl font-heading font-bold text-primary">Patrick KENNE</h1>
+        {/* <span></span> */}
+        <ul className="hidden md:flex space-x-6 transition-all">
           <li><a href="#about" className="hover:text-primary transition-colors">About</a></li>
           <li><a href="#experience" className="hover:text-primary transition-colors">Experience</a></li>
           <li><a href="#skills" className="hover:text-primary transition-colors">Skills</a></li>
           <li><a href="#projects" className="hover:text-primary transition-colors">Projects</a></li>
         </ul>
         <button onClick={handleToggleMenu} className="md:hidden text-gray-600 hover:text-primary">
-          <i className={`fas ${showMenu ? 'fa-close' : 'fa-bars'} text-2xl`}></i>
+          <i className={`fas ${showMenu ? 'fa-close' : 'fa-bars'} text-2xl transition-all`}></i>
         </button>
       </nav>
-      <div id="mobile-menu" className="hidden md:hidden bg-white pb-4">
+      <div id="mobile-menu" className="hidden md:hidden bg-white pb-4 transition-all">
         <ul className="flex flex-col items-center space-y-4">
           <li><a href="#about" className="hover:text-primary transition-colors">About</a></li>
           <li><a href="#experience" className="hover:text-primary transition-colors">Experience</a></li>
