1d4a7f3cd295bbe965344c64e027d3606fc377cd
patson120 <patrick1kenne@gmail.com>
2024-10-17 08:49:22 +0100

    Update de route.ts 1

--- src/app/api/contacts/route.ts
+++ src/app/api/contacts/route.ts
@@ -6,18 +6,18 @@ import { log } from "console";
 
 // Handles POST requests to /api/contacts
 export function POST(request: Request) {
-    const getBody = async () => {
-        return await request.json()
-    }
-    const getMailStatus = async (body: any) => {
-        const hookSenMail = useSendMail()
-        return await hookSenMail.send(
-            body.email,
-            body.object,
-            body.message,
-            `${body.nom} ${body.prenom}`,
-        )
-    }
+    // const getBody = async () => {
+    //     return await request.json()
+    // }
+    // const getMailStatus = async (body: any) => {
+    //     const hookSenMail = useSendMail()
+    //     return await hookSenMail.send(
+    //         body.email,
+    //         body.object,
+    //         body.message,
+    //         `${body.nom} ${body.prenom}`,
+    //     )
+    // }
     try {
         return NextResponse.json({ success: true, status: STATUS.CREATED, result: null }, { status: STATUS.CREATED })
         // getBody().then(body => {
